46fa7d14edf9885010b9a0259dd61e3a2ec37007
Sa <Samuel@akatsukigroup.com>
2019-11-19 17:34:23 +0800

    加入UI管理

--- project/assets/Script/Camera.ts
+++ project/assets/Script/Camera.ts
@@ -7,10 +7,15 @@ export default class Camera extends cc.Component {
     @property(cc.Node)
     player: cc.Node = null;
 
+    @property(cc.Node)
+    UI = null;
+
     start () {
     }
 
     update (dt) {
         this.node.x = this.player.position.x;
+        this.UI.x = this.node.x;
+
     }
 }
