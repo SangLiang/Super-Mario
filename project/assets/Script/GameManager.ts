f2ff44931d1fbbd73ee2ac7915f474e2f78ffc26
Sa <Samuel@akatsukigroup.com>
2019-11-19 17:51:53 +0800

    加入玩家的生命与分数的管理

--- project/assets/Script/GameManager.ts
+++ project/assets/Script/GameManager.ts
@@ -4,6 +4,13 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class GameManager extends cc.Component {
 
+    @property(cc.Label)
+    score= null
+
+    @property(cc.Label)
+    HP = null;
+    
+
     onLoad () {
         cc.director.getPhysicsManager().enabled = true;
 
